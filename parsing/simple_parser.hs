d9b245dd61c33ca63890fcd120d31eccd332ba0c
Sunil Udupi <udpsunil@gmail.com>
2020-08-05 22:00:55 +0530

    Added ListVal data type. But this haskell tut is going too deep I will start with Learn You a Haskell.# Please enter the commit message for your changes. Lines starting

--- parsing/simple_parser.hs
+++ parsing/simple_parser.hs
@@ -1,16 +1,34 @@
-import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-readExpr :: String -> String
-readExpr input = 
-    case parse symbol "listp" input of
-        Left err -> "No match: " ++ show err
-        Right val -> "Found value"
+spaces :: Parser ()
+spaces = skipMany1 space
 
-main :: IO()
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+readExpr :: String -> String
+readExpr input =
+  case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+main :: IO ()
 main = do
-    (expr:_) <- getArgs
-    putStrLn (readExpr expr)
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
