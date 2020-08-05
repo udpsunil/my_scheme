8db246f8434e693a2839cdc47bc8220a0085003f
Sunil Udupi <udpsunil@gmail.com>
2020-08-05 20:40:06 +0530

    Exercise 3: Prompt for a name and print it

--- first_step/main.hs
+++ first_step/main.hs
@@ -3,8 +3,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-     args <- getArgs
-     let num1 = read (args !! 0) :: Int
-     let num2 = read (args !! 1) :: Int
-     let sum = num1 + num2
-     putStrLn ("Hello, Sum is " ++ show sum)
+     putStrLn("Enter your name: ")
+     name <- getLine
+     putStrLn ("Hello, your name is " ++ name)
