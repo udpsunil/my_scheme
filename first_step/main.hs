ef66e262380c759376d7a9c73eb5a90fc5147e5c
Sunil Udupi <udpsunil@gmail.com>
2020-08-05 20:38:12 +0530

    Exercise 2: Add 2 numbers passed in command line

--- first_step/main.hs
+++ first_step/main.hs
@@ -3,5 +3,8 @@ import System.Environment
 
 main :: IO ()
 main = do
-     args <- getArgs     
-     putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
+     args <- getArgs
+     let num1 = read (args !! 0) :: Int
+     let num2 = read (args !! 1) :: Int
+     let sum = num1 + num2
+     putStrLn ("Hello, Sum is " ++ show sum)
